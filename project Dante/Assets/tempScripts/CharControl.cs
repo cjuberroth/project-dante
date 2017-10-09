21a256f9ee99fe8cc518250c4f774326dc48b85c
Unknown <cuberroth@gmail.com>
2017-10-09 11:14:02 -0500

    researching scripting

--- project Dante/Assets/tempScripts/CharControl.cs
+++ project Dante/Assets/tempScripts/CharControl.cs
@@ -7,4 +7,5 @@ public class CharControl : MonoBehaviour
     //reload took me back to the original...
     //was that because of git?
     //adding comment to test git
+    //var rch = Physics2D.Raycast()//vector2 origin, vector2 distance, float distance, int layerMask
 }
